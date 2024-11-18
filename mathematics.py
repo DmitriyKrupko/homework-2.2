0d6918d99c13e51574d4b617c905eb7e72cadfc9
Makoto <dimonkrupko@yandex.ru>
2024-11-18 19:15:04 +0300

    change_stroka

--- mathematics.py
+++ mathematics.py
@@ -4,5 +4,5 @@ print("Сумма чисел: ", first + second,
     "\n" "Разность чисел: ", first - second, 
     "\n" "Результат умножения: ", first * second, 
     "\n" "Частное от деления чисел: ", first // second, 
-    "\n" "Остаток от деления: ", first % second, 
+    "\n" "Результат деления: ", first % second, 
     "\n" "Число а в степени b", first ** second )
