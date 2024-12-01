a64f24f8d467d213e0d2baf593339e975037b848
Makoto <dimonkrupko@yandex.ru>
2024-12-02 00:40:14 +0300

    add\new_files

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from math import math
 try:
     first = int(input("Введите число a: "))
 except ValueError:
@@ -8,13 +9,4 @@ try:
 except ValueError:
     print('Не ну ты смог выбесить даже строчку в командной строке... Введи ЧИСЛО!!!')
     second = int(input("Введите число b: "))
-try:
-    print("Сумма чисел: ", first + second, 
-    "\n" "Разность чисел: ", first - second, 
-    "\n" "Результат умножения: ", first * second, 
-    "\n" "Частное от деления чисел: ", first // second, 
-    "\n" "Результат деления: ", first % second, 
-    "\n" "Число а в степени b", first ** second )
-except ZeroDivisionError:
-        print('Не ну ты совсем ёбобо, на ноль делить')
-        
+math(first, second)
---
+++ math/math.py
@@ -0,0 +1,8 @@
+from . .main import first, second
+def math(first, second):
+    print("Сумма чисел: ", first + second, 
+    "\n" "Разность чисел: ", first - second, 
+    "\n" "Результат умножения: ", first * second, 
+    "\n" "Частное от деления чисел: ", first // second, 
+    "\n" "Результат деления: ", first % second, 
+    "\n" "Число а в степени b", first ** second )
