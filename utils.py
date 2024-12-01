5b85ee2cde86085b79addcb7833c2ac66640c0b8
Makoto <dimonkrupko@yandex.ru>
2024-12-02 01:07:14 +0300

    edit\utils

--- utils.py
+++ utils.py
@@ -1,10 +1,8 @@
 from main import first, second
 def mathematics(first, second):
-    try:
         print("Сумма чисел: ", first + second, 
         "\n" "Разность чисел: ", first - second, 
         "\n" "Результат умножения: ", first * second, 
         "\n" "Частное от деления чисел: ", first // second, 
         "\n" "Результат деления: ", first % second, 
         "\n" "Число а в степени b", first ** second ) 
-    
